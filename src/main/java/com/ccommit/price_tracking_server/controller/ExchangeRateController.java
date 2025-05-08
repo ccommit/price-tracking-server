433c4a8e61398f1deb6e788d3e5ed9ffba65c921
gamsayeon <aud4551@naver.com>
2025-05-08 15:54:56 +0900

    ENH: 환율 API 연동 확인

--- src/main/java/com/ccommit/price_tracking_server/controller/ExchangeRateController.java
+++ src/main/java/com/ccommit/price_tracking_server/controller/ExchangeRateController.java
@@ -7,11 +7,11 @@ import com.ccommit.price_tracking_server.enums.SuccessDetailMessage;
 import com.ccommit.price_tracking_server.service.serviceImpl.ExchangeRateServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Log4j2
 @RestController
@@ -20,12 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeRateController {
     private final ExchangeRateServiceImpl exchangeRateService;
 
+    //환율 확인용 api (추후 삭제 예정)
     @GetMapping("/exchange-rate/{currency}")
-    public ResponseEntity<CommonResponse<ExchangeRateDTO>> getExchangeRate(@PathVariable(value = "currency") CurrencyCode currency) {
-
+    public ResponseEntity<CommonResponse<ExchangeRateDTO>> getExchangeRate(@PathVariable(value = "currency") CurrencyCode currency,
+                                                                           @RequestParam(value = "date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        // 환율 조회 API는 삭제 예정이라 SUCCESS_PRODUCT_SELECTED으로 성공 메시지 대체
         SuccessDetailMessage message = SuccessDetailMessage.SUCCESS_PRODUCT_SELECTED;
         CommonResponse<ExchangeRateDTO> response = new CommonResponse<>(message.name(), message.getMessage(),
-                exchangeRateService.getExchangeRates(currency), 0);
+                exchangeRateService.getExchangeRates(currency, date), 0);
         return ResponseEntity.ok(response);
     }
 }
